349d36efd5ce1ae375ea624de267d0401d27a5a2
Andrzej Wiśniewski <andrzej.wis96@gmail.com>
2025-02-12 15:24:55 +0100

    validation

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,14 +1,40 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/book.jpg",
+        creator: {
+          name: "Author",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Author" },
+      createdAt: new Date(),
+    },
   });
 };
